7f986b57c187f004ad1d69279affe22b0caf2065
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-07-27 02:46:08 +0700

    basic

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { allPosts } from "contentlayer/generated";
+import { notFound } from "next/navigation";
+import PostView from "./PostView";
+
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) notFound();
+  return {
+    title: post.title,
+  };
+};
+
+const PostLayout = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) notFound();
+
+  return <PostView post={post} />;
+};
+
+export default PostLayout;
