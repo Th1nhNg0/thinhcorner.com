ee47e5ee69df72badc547d278ea6fc60e7d2b8a6
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-07-27 04:00:47 +0700

    great

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,8 +1,13 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import readingTime from "reading-time";
+import rehypeKatex from "rehype-katex";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: `**/*.md`,
+  contentType: "mdx",
   fields: {
     title: { type: "string", required: true },
     date: { type: "date", required: true },
@@ -12,10 +17,18 @@ export const Post = defineDocumentType(() => ({
       type: "string",
       resolve: (post) => `/posts/${post._raw.flattenedPath}`,
     },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw),
+    },
   },
 }));
 
 export default makeSource({
   contentDirPath: "contents/posts",
   documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
