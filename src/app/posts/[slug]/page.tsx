754044680741c23630742443047a7d5f3573e91b
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-07-28 11:23:08 +0700

    remove package

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
-import { BlogPosting, WithContext } from "schema-dts";
 import PostView from "./PostView";
-
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
@@ -18,25 +16,8 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   if (!post) notFound();
 
-  const blogPosting: WithContext<BlogPosting> = {
-    "@context": "https://schema.org",
-    "@type": "BlogPosting",
-    headline: post.title,
-    datePublished: post.date,
-    dateModified: post.date,
-    author: {
-      "@type": "Person",
-      name: "Thịnh Ngô",
-    },
-    wordCount: post.readingTime.words,
-  };
-
   return (
     <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(blogPosting) }}
-      />
       <PostView post={post} />
     </>
   );
