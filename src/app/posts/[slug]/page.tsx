b500ffaba283b4474ca036e3b5c21148df5161b1
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-07-31 10:46:02 +0700

    opengraph

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
 import PostView from "./PostView";
+
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
@@ -9,6 +10,17 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   if (!post) return { title: "Not found" };
   return {
     title: post.title,
+    openGraph: {
+      title: post.title,
+      images: [
+        {
+          url: "https://thinhcorner.com/images/socialbg.png",
+          width: 1920,
+          height: 1080,
+          alt: "Thịnh's Corner",
+        },
+      ],
+    },
   };
 };
 
