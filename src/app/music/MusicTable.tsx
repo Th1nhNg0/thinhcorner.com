e437861d7f53d8a74034e3d78e3c0c4641b55a54
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-01-29 16:47:24 +0700

    Add recently played tracks to MusicPage component

--- src/app/music/MusicTable.tsx
+++ src/app/music/MusicTable.tsx
@@ -1,43 +1,69 @@
 "use client";
-import { TopTrack } from "@/lib/spotify";
-import React from "react";
-import { formatDistance } from "date-fns";
+import { RecentlyPlayedTrack, TopTrack } from "@/lib/spotify";
 import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
 
-export default function MusicTable({ tracks }: { tracks: TopTrack[] }) {
-  const rows = tracks.map((track) => (
-    <tr key={track.id}>
-      <td className="border-t py-3">
-        <Link
-          href={track.songUrl}
-          target="_blank"
-          className="flex gap-5 items-center font-bold hover:underline"
-        >
-          <picture className="w-20 h-20 hidden md:block">
-            <img src={track.imageUrl} alt={track.title} />
-          </picture>
-          <span className="line-clamp-1">{track.title}</span>
-        </Link>
-      </td>
-      <td className="border-t py-3">
-        {track.artist.map((e, i) => (
-          <Link
-            key={e.id}
-            href={e.external_urls.spotify}
-            target="_blank"
-            className="hover:underline"
-          >
-            {e.name}
-            {i !== track.artist.length - 1 ? ", " : ""}
-          </Link>
-        ))}
-      </td>
-    </tr>
-  ));
-
+export default function MusicTable({
+  tracks,
+  recentTracks,
+}: {
+  tracks: TopTrack[];
+  recentTracks: RecentlyPlayedTrack[];
+}) {
   return (
     <div>
-      <h1 className="text-5xl mb-5 font-bold">Spotify Top Tracks</h1>
+      <h1 className="mb-5 text-5xl font-bold">My Music</h1>
+      <h2 className="mb-5 text-4xl font-bold">Recently Played</h2>
+      <p className="text-xl">
+        These are the songs I&apos;ve listened to most recently on Spotify.
+      </p>
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th className="text-left">Track</th>
+            <th className="text-left">Artist</th>
+            <th className="text-left">Played At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recentTracks.map((track) => (
+            <tr key={track.id}>
+              <td className="py-3 border-t">
+                <Link
+                  href={track.songUrl}
+                  target="_blank"
+                  className="flex items-center gap-5 font-bold hover:underline"
+                >
+                  <picture className="hidden w-20 h-20 md:block">
+                    <img src={track.imageUrl} alt={track.title} />
+                  </picture>
+                  <span className="line-clamp-1">{track.title}</span>
+                </Link>
+              </td>
+              <td className="py-3 border-t">
+                {track.artist.map((e, i) => (
+                  <Link
+                    key={e.id}
+                    href={e.external_urls.spotify}
+                    target="_blank"
+                    className="hover:underline"
+                  >
+                    {e.name}
+                    {i !== track.artist.length - 1 ? ", " : ""}
+                  </Link>
+                ))}
+              </td>
+              <td className="py-3 border-t">
+                {formatDistanceToNow(new Date(track.played_at), {
+                  addSuffix: true,
+                })}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <hr className="my-10" />
+      <h2 className="mb-5 text-4xl font-bold">Top Tracks</h2>
       <p className="text-xl">
         These are my top tracks on Spotify, based on the last 4 weeks of
         listening.
@@ -49,7 +75,41 @@ export default function MusicTable({ tracks }: { tracks: TopTrack[] }) {
             <th className="text-left">Artist</th>
           </tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody>
+          {tracks.map((track) => (
+            <tr key={track.id}>
+              <td className="py-3 border-t">
+                <Link
+                  href={track.songUrl}
+                  target="_blank"
+                  className="flex items-center gap-5 font-bold hover:underline"
+                >
+                  <picture className="hidden w-20 h-20 md:block aspect-square">
+                    <img
+                      src={track.imageUrl}
+                      alt={track.title}
+                      className="w-full h-full"
+                    />
+                  </picture>
+                  <span className="line-clamp-1">{track.title}</span>
+                </Link>
+              </td>
+              <td className="py-3 border-t">
+                {track.artist.map((e, i) => (
+                  <Link
+                    key={e.id}
+                    href={e.external_urls.spotify}
+                    target="_blank"
+                    className="hover:underline"
+                  >
+                    {e.name}
+                    {i !== track.artist.length - 1 ? ", " : ""}
+                  </Link>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
