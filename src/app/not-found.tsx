7894ab5d5d54004450958e42d5d09bd995c17c5c
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-02-25 17:54:22 +0700

    Fix formatting and add missing information

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="grid min-h-full px-6 py-24 bg-white place-items-center sm:py-32 lg:px-8">
+    <div className="grid min-h-full px-6 py-24 place-items-center sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-gray-900">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
