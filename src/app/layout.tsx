c5e8cbd82411bab7fb15a4a0a447c558e85730cb
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-06-16 05:20:16 +0700

    chore: Add Clarity analytics script to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Navbar } from "./Navbar";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -62,6 +63,15 @@ export default function RootLayout({
         </main>
       </body>
       <GoogleAnalytics gaId="G-P4B7XCWCYP" />
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "msjdwtwwqu");
+        `}
+      </Script>
     </html>
   );
 }
