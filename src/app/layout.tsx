7969ebbde3834d1064963e47321314e7a294bbd2
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-08-15 14:05:50 +0700

    chore: Update layout.tsx and tailwind.config.ts
    Refactor layout.tsx to improve code readability and remove unnecessary code. Update tailwind.config.ts to adjust padding values for the container.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,12 +51,15 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn(
-        "text-black bg-white dark:text-white dark:bg-[#111010] ",
-        fontSans.variable
-      )}
+      suppressHydrationWarning
+      className="text-black bg-white dark:text-white dark:bg-[#111010] "
     >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto ">
+      <body
+        className={cn(
+          "antialiased font-sans max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto ",
+          fontSans.variable
+        )}
+      >
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
