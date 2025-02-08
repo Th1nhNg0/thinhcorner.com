d91dcb52c3aeea1f6b5eaac4a1318a1e781fcdf2
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-02-08 23:46:49 +0700

    Add music page with Spotify integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { remarkModifiedTime } from "./remark/remark-modified-time.mjs";
 import { remarkReadingTime } from "./remark/remark-reading-time.mjs";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://thinhcorner.com",
+  adapter: vercel({
+    imageService: true,
+
+    isr: {
+      expiration: 60 * 60 * 24,
+    },
+  }),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
