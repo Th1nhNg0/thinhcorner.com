1cdf306a8b85702773e4667adef3be72fde658c2
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-02-08 18:36:34 +0700

    Add robots.txt generation and timeline component with dynamic entries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import { remarkModifiedTime } from "./remark/remark-modified-time.mjs";
 import { remarkReadingTime } from "./remark/remark-reading-time.mjs";
-import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3000",
-  integrations: [],
+  site: "https://thinhcorner.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
-    remarkPlugins: [
-      remarkReadingTime,
-      remarkModifiedTime,
-      remarkMath,
-      remarkGfm,
-    ],
+    remarkPlugins: [remarkReadingTime, remarkModifiedTime, remarkMath],
     rehypePlugins: [rehypeKatex],
+    gfm: true,
   },
 });
