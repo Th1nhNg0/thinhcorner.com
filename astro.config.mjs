3e3b149cd851fc0e06416c128161c46c4530d707
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-02-08 14:23:34 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import { remarkReadingTime } from "./remark/remark-reading-time.mjs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:3000",
+  integrations: [],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+});
