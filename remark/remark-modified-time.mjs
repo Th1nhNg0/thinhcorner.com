1964857879ba4c2cc65cd13576c1c2ccc5907426
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-07-08 11:17:57 +0700

    feat: add custom 404 page with navigation and fun fact

--- remark/remark-modified-time.mjs
+++ remark/remark-modified-time.mjs
@@ -1,9 +1,20 @@
-import { execSync } from "child_process";
+// use git
+// import { execSync } from "child_process";
+// export function remarkModifiedTime() {
+//   return function (tree, file) {
+//     const filepath = file.history[0];
+//     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+//     file.data.astro.frontmatter.lastModified = result.toString();
+//   };
+// }
 
+
+// use file system
+import { statSync } from "fs";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    file.data.astro.frontmatter.lastModified = result.toString();
+    const result = statSync(filepath);
+    file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
   };
 }
