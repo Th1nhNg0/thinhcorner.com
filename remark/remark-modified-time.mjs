d307badd58c1209ec35da229473e9a29dffe4a4c
Th1nhNg0 <ngophuthinh2001@gmail.com>
2025-07-08 11:24:04 +0700

    refactor: switch to using git for last modified time retrieval

--- remark/remark-modified-time.mjs
+++ remark/remark-modified-time.mjs
@@ -1,20 +1,20 @@
 // use git
-// import { execSync } from "child_process";
-// export function remarkModifiedTime() {
-//   return function (tree, file) {
-//     const filepath = file.history[0];
-//     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-//     file.data.astro.frontmatter.lastModified = result.toString();
-//   };
-// }
-
-
-// use file system
-import { statSync } from "fs";
+import { execSync } from "child_process";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const result = statSync(filepath);
-    file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
+    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
+
+
+// use file system
+// import { statSync } from "fs";
+// export function remarkModifiedTime() {
+//   return function (tree, file) {
+//     const filepath = file.history[0];
+//     const result = statSync(filepath);
+//     file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
+//   };
+// }
