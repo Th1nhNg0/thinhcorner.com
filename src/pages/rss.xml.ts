dc9192694ba3b2586e4a42c12c8212d7466ef543
th1nhng0 <ngophuthinh2001@gmail.com>
2025-09-09 17:59:20 +0700

    feat: enhance SEO capabilities by adding extended metadata and structured data support across various components

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,17 +3,19 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context: any) {
-  const blog = await getCollection("blog");
+  const blog = (await getCollection("blog")).filter((p) => !p.data.draft);
 
   return rss({
     title: SITE.title,
     description: SITE.description,
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
-      link: `/blog/${post.id}`,
-    })),
+    items: blog
+      .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.date,
+        description: post.data.description,
+        link: `/blog/${post.id}`,
+      })),
   });
 }
